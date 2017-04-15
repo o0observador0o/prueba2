1161ff0b6ef11ac6f0b68d7de759e5bb1521131d
o0observador0o <yony_ordonez@usmp.pe>
2017-04-15 16:15:21 -0500

    segundo commit

---
+++ prueba2/prueba2/prueba22.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba2
+{
+    public class prueba22
+    {
+        public string Nombre { get; set; }
+    }
+}
